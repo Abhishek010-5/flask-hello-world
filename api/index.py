28eddb535e2ab87b51461101d9c3ebea2b849e8c
Abhishek010-5 <0105ds211002@gmail.com>
2024-10-06 10:04:54 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    question = questions[0]
-    to_return = question["April"]
-    return jsonify(to_return)
+    # question = questions[0]
+    # to_return = question["April"]
+    return jsonify(questions)
 
 @app.route('/about')
 def about():
